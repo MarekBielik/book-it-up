44181ed24dd4c5460454b49b290e8e7cd424d93c
marek <bielik@zulapost.net>
2016-03-22 15:57:43 +0000

    full support for users & roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserTableSeeder::class);
+        //create users
+        DB::table('users')->insert(['name' => 'marek', 'email' => 'admin@mail.com', 'password' => bcrypt('password'),]);
+        DB::table('users')->insert(['name' => 'marek', 'email' => 'librarian@mail.com', 'password' => bcrypt('password'),]);
+        DB::table('users')->insert(['name' => 'marek', 'email' => 'customer@mail.com', 'password' => bcrypt('password'),]);
+
+
+        //define roles
+        $admin = new Role();
+        $admin->name         = 'admin';
+        $admin->display_name = 'System Administrator'; // optional
+        $admin->description  = 'User is allowed to manage and edit other users'; // optional
+        $admin->save();
+
+        $librarian = new Role();
+        $librarian->name = 'librarian';
+
+        $customer = new Role();
+        $customer->name = 'customer';
+
+
+        //assign roles
+        User::where('email', '=', 'admin@mail.com')->first()->attachRole($admin);
+        User::where('email', '=', 'librarian@mail.com')->first()->attachRole($librarian);
+        User::where('email', '=', 'customer@mail.com')->first()->attachRole($customer);
     }
 }
